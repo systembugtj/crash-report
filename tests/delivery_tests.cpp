715f541c2fc85c29c93aff9125e4ff2c5897e034
yeshunping@gmail.com <yeshunping@gmail.com@9307afbf-8b4c-5d34-949b-c69a0924eb0b>
2011-03-13 05:47:05 +0000

    1,fix code style problem 2,delete support for smtp and smapi procotol 3,delete too many infomation when sending a crash report 3, delete detail_dlg and file_preview_ctrl, since they are not needed for users

--- tests/delivery_tests.cpp
+++ tests/delivery_tests.cpp
@@ -59,8 +59,6 @@ void Test_HttpDelivery_legacy_encoding()
   info.flags = CR_INST_NO_GUI;
   info.crash_server_url = _T("localhost/crashrpt.php"); // Use HTTP address for delivery 
   info.priorities[CR_HTTP] = 0;
-  info.priorities[CR_SMTP] = CR_NEGATIVE_PRIORITY;
-  info.priorities[CR_SMAPI] = CR_NEGATIVE_PRIORITY;
   info.save_dir = sTmpFolder;
   int nInstResult = crInstall(&info);
   TEST_ASSERT(nInstResult==0);
@@ -110,8 +108,6 @@ void Test_HttpDelivery_binary_encoding()
   info.flags = CR_INST_NO_GUI|CR_INST_HTTP_BINARY_ENCODING;
   info.crash_server_url = _T("localhost/crashrpt.php"); // Use HTTP address for delivery 
   info.priorities[CR_HTTP] = 0;
-  info.priorities[CR_SMTP] = CR_NEGATIVE_PRIORITY;
-  info.priorities[CR_SMAPI] = CR_NEGATIVE_PRIORITY;  
   info.save_dir = sTmpFolder;
   int nInstResult = crInstall(&info);
   TEST_ASSERT(nInstResult==0);
@@ -129,170 +125,6 @@ void Test_HttpDelivery_binary_encoding()
   DWORD dwExitCode = 1;
   GetExitCodeProcess(exc.hSenderProcess, &dwExitCode);
   TEST_ASSERT(dwExitCode==0); // Exit code should be zero
-  
-  __TEST_CLEANUP__;  
-
-  // Uninstall
-  crUninstall();  
-
-  // Delete tmp folder
-  Utility::RecycleFile(sTmpFolder, TRUE);
-}
-
-REGISTER_TEST(Test_SmtpDelivery);
-void Test_SmtpDelivery()
-{ 
-  CString sAppDataFolder;
-  CString sExeFolder;
-  CString sTmpFolder;
-
-  // Create a temporary folder  
-  Utility::GetSpecialFolder(CSIDL_APPDATA, sAppDataFolder);
-  sTmpFolder = sAppDataFolder+_T("\\CrashRpt");
-  BOOL bCreate = Utility::CreateFolder(sTmpFolder);
-  TEST_ASSERT(bCreate);
-
-  // Install crash handler for the main thread
-
-  CR_INSTALL_INFO info;
-  memset(&info, 0, sizeof(CR_INSTALL_INFO));
-  info.size = sizeof(CR_INSTALL_INFO);
-  info.application_version = _T("1.0.0"); // Specify app version, otherwise it will fail.
-  info.flags = CR_INST_NO_GUI|CR_INST_HTTP_BINARY_ENCODING;
-  info.email_address = _T("test@localhost");
-  info.email_subject = _T("Crash Report Whooaaa!!!");
-  info.email_text = _T("And some text in the email body...");
-  info.priorities[CR_HTTP] = CR_NEGATIVE_PRIORITY;
-  info.priorities[CR_SMTP] = 0;
-  info.priorities[CR_SMAPI] = CR_NEGATIVE_PRIORITY;  
-  info.save_dir = sTmpFolder;
-  int nInstResult = crInstall(&info);
-  TEST_ASSERT(nInstResult==0);
-    
-  // Generate and send error report
-  CR_EXCEPTION_INFO exc;
-  memset(&exc, 0, sizeof(CR_EXCEPTION_INFO));
-  exc.cb = sizeof(CR_EXCEPTION_INFO);
-  int nResult2 = crGenerateErrorReport(&exc);
-  TEST_ASSERT(nResult2==0);
-
-  // Wait until CrashSender exits and check exit code
-  WaitForSingleObject(exc.hSenderProcess, INFINITE);
-
-  DWORD dwExitCode = 1;
-  GetExitCodeProcess(exc.hSenderProcess, &dwExitCode);
-  TEST_ASSERT(dwExitCode==0); // Exit code should be zero
-  
-  __TEST_CLEANUP__;  
-
-  // Uninstall
-  crUninstall();  
-
-  // Delete tmp folder
-  Utility::RecycleFile(sTmpFolder, TRUE);
-}
-
-REGISTER_TEST(Test_SmtpDelivery_proxy);
-void Test_SmtpDelivery_proxy()
-{ 
-  CString sAppDataFolder;
-  CString sExeFolder;
-  CString sTmpFolder;
-
-  // Create a temporary folder  
-  Utility::GetSpecialFolder(CSIDL_APPDATA, sAppDataFolder);
-  sTmpFolder = sAppDataFolder+_T("\\CrashRpt");
-  BOOL bCreate = Utility::CreateFolder(sTmpFolder);
-  TEST_ASSERT(bCreate);
-
-  // Install crash handler for the main thread
-
-  CR_INSTALL_INFO info;
-  memset(&info, 0, sizeof(CR_INSTALL_INFO));
-  info.size = sizeof(CR_INSTALL_INFO);
-  info.application_version = _T("1.0.0"); // Specify app version, otherwise it will fail.
-  info.flags = CR_INST_NO_GUI|CR_INST_HTTP_BINARY_ENCODING;
-  info.email_address = _T("test@localhost");
-  info.smtp_proxy = _T("127.0.0.1:25");
-  info.email_subject = _T("Crash Report Whooaaa!!!");
-  info.email_text = _T("And some text in the email body...");
-  info.priorities[CR_HTTP] = CR_NEGATIVE_PRIORITY;
-  info.priorities[CR_SMTP] = 0;
-  info.priorities[CR_SMAPI] = CR_NEGATIVE_PRIORITY;  
-  info.save_dir = sTmpFolder;
-  int nInstResult = crInstall(&info);
-  TEST_ASSERT(nInstResult==0);
-    
-  // Generate and send error report
-  CR_EXCEPTION_INFO exc;
-  memset(&exc, 0, sizeof(CR_EXCEPTION_INFO));
-  exc.cb = sizeof(CR_EXCEPTION_INFO);
-  int nResult2 = crGenerateErrorReport(&exc);
-  TEST_ASSERT(nResult2==0);
-
-  // Wait until CrashSender exits and check exit code
-  WaitForSingleObject(exc.hSenderProcess, INFINITE);
-
-  DWORD dwExitCode = 1;
-  GetExitCodeProcess(exc.hSenderProcess, &dwExitCode);
-  TEST_ASSERT(dwExitCode==0); // Exit code should be zero
-  
-  __TEST_CLEANUP__;  
-
-  // Uninstall
-  crUninstall();  
-
-  // Delete tmp folder
-  Utility::RecycleFile(sTmpFolder, TRUE);
-}
-
-
-// This test tries to send report in silent mode over SMAPI.
-// Since SMAPI is not available in silent mode, test passes when delivery fails.
-REGISTER_TEST(Test_SMAPI_Delivery);
-void Test_SMAPI_Delivery()
-{ 
-  CString sAppDataFolder;
-  CString sExeFolder;
-  CString sTmpFolder;
-
-  // Create a temporary folder  
-  Utility::GetSpecialFolder(CSIDL_APPDATA, sAppDataFolder);
-  sTmpFolder = sAppDataFolder+_T("\\CrashRpt");
-  BOOL bCreate = Utility::CreateFolder(sTmpFolder);
-  TEST_ASSERT(bCreate);
-
-  // Install crash handler for the main thread
-
-  CR_INSTALL_INFO info;
-  memset(&info, 0, sizeof(CR_INSTALL_INFO));
-  info.size = sizeof(CR_INSTALL_INFO);
-  info.application_version = _T("1.0.0"); // Specify app version, otherwise it will fail.
-  info.flags = CR_INST_NO_GUI|CR_INST_HTTP_BINARY_ENCODING;
-  info.email_address = _T("test@gmail.com");
-  info.email_subject = _T("Crash Report Whooaaa!!!");
-  info.email_text = _T("And some text in the email body...");
-  info.priorities[CR_HTTP] = CR_NEGATIVE_PRIORITY;
-  info.priorities[CR_SMTP] = CR_NEGATIVE_PRIORITY;
-  info.priorities[CR_SMAPI] = 0;  
-  info.save_dir = sTmpFolder;
-  int nInstResult = crInstall(&info);
-  TEST_ASSERT(nInstResult==0);
-    
-  // Generate and send error report
-  CR_EXCEPTION_INFO exc;
-  memset(&exc, 0, sizeof(CR_EXCEPTION_INFO));
-  exc.cb = sizeof(CR_EXCEPTION_INFO);
-  int nResult2 = crGenerateErrorReport(&exc);
-  TEST_ASSERT(nResult2==0);
-
-  // Wait until CrashSender exits and check exit code
-  WaitForSingleObject(exc.hSenderProcess, INFINITE);
-
-  DWORD dwExitCode = 1;
-  GetExitCodeProcess(exc.hSenderProcess, &dwExitCode);
-  // Since SMAPI is not available in silent mode, test passes when delivery fails.
-  TEST_ASSERT(dwExitCode!=0); // Exit code should be non-zero
   
   __TEST_CLEANUP__;  
 
